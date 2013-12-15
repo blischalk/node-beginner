7cd2c40a28b2a1f1faa81f087ff6ce48b22166b2
Brett Lischalk <brett@brettlischalk.com>
2013-12-15 16:01:10 -0600

    Blocking the node.js process.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,5 +1,11 @@
 exports.start = function(){
   console.log("Request handler 'start' was called.");
+  function sleep(milliSeconds) {
+    var startTime = new Date().getTime();
+    while (new Date().getTime() < startTime + milliSeconds);
+  }
+
+  sleep(10000);
   return "Hello Start";
 }
 
